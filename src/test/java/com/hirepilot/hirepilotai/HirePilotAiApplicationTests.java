62a8ea7d5c3d7e20cbaae733e7abed1cfe0e9bb8
Sathish Kumar <satish.tech2434@gmail.com>
2026-06-27 20:08:37 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/hirepilot/hirepilotai/HirePilotAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirepilot.hirepilotai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirePilotAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
