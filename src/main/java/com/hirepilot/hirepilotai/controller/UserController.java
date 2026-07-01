a5a5b4081bef24be78a8741c9d24e770b74cff15
Sathish Kumar <satish.tech2434@gmail.com>
2026-07-01 15:27:02 +0530

    Sprint 6.1: Added Login API with BCrypt password authentication

--- src/main/java/com/hirepilot/hirepilotai/controller/UserController.java
+++ src/main/java/com/hirepilot/hirepilotai/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.hirepilot.hirepilotai.controller;
 
+import com.hirepilot.hirepilotai.dto.request.LoginRequest;
+import com.hirepilot.hirepilotai.dto.response.LoginResponse;
 import com.hirepilot.hirepilotai.entity.User;
 import com.hirepilot.hirepilotai.service.UserService;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
-import com.hirepilot.hirepilotai.dto.UserRegistrationRequest;
+import com.hirepilot.hirepilotai.dto.request.UserRegistrationRequest;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -19,5 +21,10 @@ public class UserController {
     @PostMapping("/register")
     public User registerUser(@Valid @RequestBody UserRegistrationRequest request) {
         return userService.registerUser(request);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@Valid @RequestBody LoginRequest request) {
+        return userService.login(request);
     }
 }
--- src/main/java/com/hirepilot/hirepilotai/service/UserService.java
+++ src/main/java/com/hirepilot/hirepilotai/service/UserService.java
@@ -1,11 +1,11 @@
 package com.hirepilot.hirepilotai.service;
 
-import com.hirepilot.hirepilotai.dto.UserRegistrationRequest;
+import com.hirepilot.hirepilotai.dto.request.UserRegistrationRequest;
 import com.hirepilot.hirepilotai.entity.User;
 import com.hirepilot.hirepilotai.repository.UserRepository;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
-import com.hirepilot.hirepilotai.dto.UserRegistrationRequest;
+import com.hirepilot.hirepilotai.dto.request.LoginRequest;
+import com.hirepilot.hirepilotai.dto.response.LoginResponse;
 import java.util.Optional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -32,5 +32,18 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setMobile(request.getMobile());
         return userRepository.save(user);
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        Optional<User> existingUser = userRepository.findByEmail(request.getEmail());
+        if (existingUser.isEmpty()) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        User user = existingUser.get();
+        if (!passwordEncoder.matches(request.getPassword(),user.getPassword())) {
+            throw new RuntimeException("Invalid email or password");
+        }
+        return new LoginResponse("Login Successful");
     }
 }
