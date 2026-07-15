2160099d5b3ac2b28a61e3a6d6d46d2567d06b38
Sathish Kumar <satish.tech2434@gmail.com>
2026-07-15 23:26:47 +0530

    chore: stop tracking uploaded resume files

---
+++ src/main/java/com/hirepilot/hirepilotai/dto/parser/ResumeParsedData.java
@@ -0,0 +1,77 @@
+package com.hirepilot.hirepilotai.dto;
+
+import java.util.List;
+
+public class ResumeParsedData {
+
+    private String extractedText;
+
+    private String name;
+
+    private String email;
+
+    private String mobile;
+
+    private List<String> skills;
+
+    private String experience;
+
+    private String education;
+
+    public String getExtractedText() {
+        return extractedText;
+    }
+
+    public void setExtractedText(String extractedText) {
+        this.extractedText = extractedText;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+}
---
+++ src/main/java/com/hirepilot/hirepilotai/service/ResumeParserService.java
@@ -0,0 +1,4 @@
+package com.hirepilot.hirepilotai.service;
+
+public class ResumeParserService {
+}
